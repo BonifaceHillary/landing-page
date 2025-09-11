407593018226ceef56a47b1212857925ee9200a7
Boniface Hillary <nkasiboniface@gail.com>
2025-09-11 21:53:12 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 // import React from 'react'
+import Background from "./Components/Background/Background"
+import { useState } from "react"
 
+let heroData = [
+  {text1: "Dive into", text2: "what you love"},
+  {text1: "Dive into", text2: "what you love"},
+  {text1: "Dive into", text2: "what you love"},
+]
+
+const [heroCount, setHeroCount] = useState(2);
+const [playStatus, setPlayStatus] = useState(false)
 const App = () => {
   return (
     <div>
+      <Background/>
       
     </div>
   )
