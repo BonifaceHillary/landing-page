8c6873d0bec104bb192b4d0035890d615bbf79e5
Boniface Hillary <nkasiboniface@gail.com>
2025-09-17 18:00:54 +0100

    initial commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,31 @@
 // import React from 'react'
 import './Hero.css'
+import arrow from '../../assets/arrow-icon.png'
 
-const Hero = () => {
+import play from '../../assets/play-icon.jpg'
+
+import pause from '../../assets/pause-icon.jpg'
+
+const Hero = ({heroData, setHeroCount, heroCount, setPlayStatus, playStatus}) => {
   return (
-    <div>
+    <div className='hero'>
+      <div className="hero-text">
+        <p>{heroData.text1}</p>
+        <p>{heroData.text2}</p>
+      </div>
+
+      <div className="hero-explore">
+        <p>explore the features</p>
+        <img src={arrow} alt="" />
+      </div>
+
+      <div className="hero-dot-play">
+        <ul className="hero-dots">
+          <li onClick={()=>setHeroCount(0)} className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
+          <li onClick={()=>setHeroCount(1)} className={heroCount===1?"hero-dot orange":"hero-dot"}></li>
+          <li onClick={()=>setHeroCount(2)} className={heroCount===2?"hero-dot orange":"hero-dot"}></li>
+        </ul>
+      </div>
       
     </div>
   )
